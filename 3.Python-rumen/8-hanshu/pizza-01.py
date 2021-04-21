a20a90ae0fafc9d17afb55399ec4f34ea4813f6a
wklesss <wangkai0916@live.com>
2021-04-21 08:40:58 +0800

    8.5　传递任意数量的实参

---
+++ 3.Python-rumen/8-hanshu/pizza-01.py
@@ -0,0 +1,8 @@
+def make_pizza(*toppings):
+    """概述要制作的比萨"""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
