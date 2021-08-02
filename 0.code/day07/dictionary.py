4d803327918109d615056f2f4d68dfa205e2389b
wklesss <wangkai0916@live.com>
2021-08-02 15:40:14 +0800

    跑马灯文字

--- 0.code/day07/dictionary.py
+++ 0.code/day07/dictionary.py
@@ -1,3 +1,33 @@
 # 创建字典的字面量语法
 scores ={'wk':22, 'kk':23, 'ww':24}
 print(scores)
+# 创建字典的构造器语法
+items1 = dict(one=1, two=2, three=3, four=4)
+# 通过zip函数将两个序列压成字典
+items2= dict(zip(['a', 'b', 'c'], '123'))
+# 创建字典的推导式语法
+items3 = {num: num ** 2 for num in range(1, 10)}
+print(items1, items2, items3)
+# 通过键可以获取字典中对应的值
+print(scores['wk'])
+print(scores['kk'])
+# 对字典中所有键值进行遍历
+for key in scores:
+    print(f'{key}: {scores[key]}')
+ # 更新字典中的元素
+scores['kk'] = 33
+scores['ww'] = 44
+scores.update(www=55, kkk=66)
+print(scores)
+if 'kk' in scores:
+    print(scores['kk'])
+print(scores.get('kk'))
+# get方法也是通过键获取对应的值但是可以设置默认值
+print(scores.get('kk', 55))
+# 删除字典中的元素
+print(scores.popitem())
+print(scores.popitem())
+print(scores.pop('kk', 100))
+# 清空字典
+scores.clear()
+print(scores)
---
+++ 0.code/day07/run_txt.py
@@ -0,0 +1,14 @@
+import os, time
+
+def main():
+    content = '北京欢迎你，为你开天辟地......'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] +content[0]
+        
+if __name__ == '__main__':
+    main()
